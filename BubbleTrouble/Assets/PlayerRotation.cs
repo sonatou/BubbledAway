a5e4fdebe4872bfa3c0aeafd55a74726638cf34d
Allan Almeida <92676069+sonatou@users.noreply.github.com>
2024-04-12 18:03:49 -0300

    upload base game

---
+++ BubbleTrouble/Assets/PlayerRotation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private float force = 1f;
+    [SerializeField] private float maxForce = 15f;
+    private KeyCode shootKey = KeyCode.Mouse0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = direction;
+
+        if(Input.GetKey(shootKey))
+        {
+            force += Time.deltaTime * 4;
+        }
+        if (Input.GetKeyUp(shootKey) || force >= maxForce)
+        {
+            playerRb.AddForce(transform.up * force, ForceMode2D.Impulse);
+            force = 1f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
